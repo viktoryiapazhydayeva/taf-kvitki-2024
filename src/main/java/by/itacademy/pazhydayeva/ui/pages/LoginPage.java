0531bc9de4c2959ed89d33ad5f9e1f5425af2215
viktoryiapazhydayeva <viktoryia_pazhydayeva@epam.com>
2024-04-24 18:25:55 +0300

    Refactor getLoginErrorMsgText method

--- src/main/java/by/itacademy/pazhydayeva/ui/pages/LoginPage.java
+++ src/main/java/by/itacademy/pazhydayeva/ui/pages/LoginPage.java
@@ -4,6 +4,10 @@ import by.itacademy.pazhydayeva.driver.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class LoginPage {
 
@@ -40,6 +44,9 @@ public class LoginPage {
     }
 
     public String getLoginErrorMsgText() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(ERROR_MSG_XPATH)));
+
         WebElement loginErrorMsg = driver.findElement(By.xpath(ERROR_MSG_XPATH));
         return loginErrorMsg.getText();
     }
