5561cd44b7867439e622622d1c06569dd823d716
viktoryiapazhydayeva <viktoryia_pazhydayeva@epam.com>
2024-04-25 21:29:38 +0300

    Refactor test classes in ui package

--- src/test/java/by/itacademy/pazhydayeva/ui/SearchUiTest.java
+++ src/test/java/by/itacademy/pazhydayeva/ui/SearchUiTest.java
@@ -1,6 +1,5 @@
 package by.itacademy.pazhydayeva.ui;
 
-import by.itacademy.pazhydayeva.ui.pages.HomePage;
 import by.itacademy.pazhydayeva.ui.pages.SearchPage;
 import by.itacademy.pazhydayeva.utils.Util;
 import org.junit.jupiter.api.Assertions;
@@ -8,24 +7,26 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static by.itacademy.pazhydayeva.ui.pages.SearchPage.EXPECTED_NO_SEARCH_RESULT_MSG;
+
 @Tag("UI")
 @Tag("Search")
 public class SearchUiTest extends BaseTest {
-    private static final String EXPECTED_NO_SEARCH_RESULT_MSG = "По вашему запросу ничего не найдено";
-    private HomePage homePage = new HomePage();
-    private SearchPage searchPage = new SearchPage();
 
     @Test
     @DisplayName("No search results")
     public void testSearchWithNoResults() {
         homePage.searchItem(Util.getRandomString(5));
+        SearchPage searchPage = new SearchPage();
         Assertions.assertEquals(EXPECTED_NO_SEARCH_RESULT_MSG, searchPage.getSearchMsgText());
     }
 
     @Test
     @DisplayName("Success search")
     public void testSearch() {
-        homePage.searchItem("славянский базар");
-        Assertions.assertTrue(searchPage.ifSearchedItemContainsQueryText("славянский базар"));
+        String searchQuery = "славянский базар";
+        homePage.searchItem(searchQuery);
+        SearchPage searchPage = new SearchPage();
+        Assertions.assertTrue(searchPage.ifSearchedItemContainsQueryText(searchQuery));
     }
 }
